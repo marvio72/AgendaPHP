f8b9e48c699c4f4d333bc9a9926cd350e8b46e33
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-04-07 14:44:34 -0500

    Formato a la pagina principal de Contactos

--- index.php
+++ index.php
@@ -27,4 +27,58 @@
     </form>
 </div>
 <!--Fin de bg-amarillo contenedor sombra-->
+
+<div class="bg-blanco contenedor sombra contactos">
+    <div class="contenedor-contactos">
+        <h2>Contactos</h2>
+
+        <input type="text" id="buscar" class="buscador sombra" placeholder="Buscar Contactos...">
+        <p class="total-contactos"><span>2</span>Contactos</p>
+
+
+        <div class="contenedor-tabla">
+            <table id="listado-contactos" class="listado-contactos">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Empresa</th>
+                        <th>Teléfono</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Marco</td>
+                        <td>Oversistemas</td>
+                        <td>3339541057</td>
+                        <td>
+                            <a href="#" class="btn-editar btn"><i class="fas fa-pen-square"></i></a>
+                            <button data-id="1" type="button" class="btn-borrar btn"><i class="fas fa-trash-alt"></i></button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Marco</td>
+                        <td>Oversistemas</td>
+                        <td>3339541057</td>
+                        <td>
+                            <a href="#" class="btn-editar btn"><i class="fas fa-pen-square"></i></a>
+                            <button data-id="1" type="button" class="btn-borrar btn"><i class="fas fa-trash-alt"></i></button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Marco</td>
+                        <td>Oversistemas</td>
+                        <td>3339541057</td>
+                        <td>
+                            <a href="#" class="btn-editar btn"><i class="fas fa-pen-square"></i></a>
+                            <button data-id="1" type="button" class="btn-borrar btn"><i class="fas fa-trash-alt"></i></button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <!--Fin de contenedor-tabla-->
+    </div>
+</div>
+<!--Fin de bg-blanco-->
 <?php include 'inc/layout/footer.php'; ?>
